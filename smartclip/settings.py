67983c8dbab2cd6c141e7fe11568a654487dea05
Ani Chan <ani.chan844@gmail.com>
2013-03-07 21:40:26 -0500

    Main app, template for home page with bootstrap

--- smartclip/settings.py
+++ smartclip/settings.py
@@ -1,4 +1,5 @@
 # Django settings for smartclip project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -7,19 +8,18 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
-MANAGERS = ADMINS
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
+        'NAME': 'smartclipdb',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+        'HOST': '',
+        'PORT': ''
+    },
 }
+
+MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -67,12 +67,12 @@ STATIC_ROOT = ''
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
+PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+    PATH + '/static',
+    )
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -108,10 +108,8 @@ ROOT_URLCONF = 'smartclip.urls'
 WSGI_APPLICATION = 'smartclip.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+    PATH + '/templates',
+    )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -120,10 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'main'
 )
 
 # A sample logging configuration. The only tangible logging
