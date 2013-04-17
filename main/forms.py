2e5f3bb53a07dd30f6459a76d3e6b3582382e762
Ani Chan <ani.chan844@gmail.com>
2013-04-17 08:03:53 -0400

    Django form for clippings

---
+++ main/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from main.models import *
+
+class ClippingForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super (ClippingForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super(ClippingForm, self).save(commit=False)
+        if self.user:
+            instance.user = self.user
+        return instance.save()
+
+    class Meta:
+        model = Clipping
+        fields = ('title', 'tags', 'source_url')
