ee1ff6c1ce59af770cbe091648a28e744d34e86b
Ani Chan <ani.chan844@gmail.com>
2013-04-17 13:17:48 -0400

    Incorporate Manpreet's style and layout changes

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('smartclip.main.views',
     url(r'^$', 'home', name='home'),
     url(r'^oauth-redirect/$', 'oauth_redirect', name='oauth_redirect'),
+    url(r'^clippings/$', 'view_clippings', name='view_clippings'),
     url(r'^auth/$', 'authenticate', name='authenticate'),
     url(r'^login/$', 'verify_login', name='verify_login')
 )
